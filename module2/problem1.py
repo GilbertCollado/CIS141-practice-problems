ed017b8dae12ded5adc8cb0c295e326b04c355aa
GILBERT COLLADO DE LA CRUZ <gilbertcolladoxd@outlook.com>
2025-04-19 12:29:43 +0900

    Update problem1.py

--- module2/problem1.py
+++ module2/problem1.py
@@ -1,1 +1,2 @@
-
+# create a program that prints Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked.
+print("Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked.")
