06fca77c0f6f9273bb5cb7e81507873224bb4cdf
GILBERT COLLADO DE LA CRUZ <gilbertcolladoxd@outlook.com>
2025-04-19 12:37:02 +0900

    Update problem3.py

--- module2/problem3.py
+++ module2/problem3.py
@@ -1,1 +1,8 @@
-
+# Create a program that prompts for the side lengths of a triangle and computes the area using Heron's formula. (https://en.wikipedia.org/wiki/Heron%27s_formula)
+import math
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+c = float(input("Enter side c: "))
+s = (a + b + c) / 2
+area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+print("Area of the triangle:", area)
