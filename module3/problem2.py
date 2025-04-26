218a037f4b97e9a3cb8e131a33c53f84f2bf7ee7
GILBERT COLLADO DE LA CRUZ <gilbertcolladoxd@outlook.com>
2025-04-26 15:45:28 +0900

    Update problem2.py

--- module3/problem2.py
+++ module3/problem2.py
@@ -1,1 +1,5 @@
-
+# Concatenation with +
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+next_age = age + 1
+print("Hello, " + name + "! You are " + str(age) + " years old. Next year, you will be " + str(next_age) + " years old.")
