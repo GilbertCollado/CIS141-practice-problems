773e85bbb4afb83b2f07a5836918a44aaf445c6c
GILBERT COLLADO DE LA CRUZ <gilbertcolladoxd@outlook.com>
2025-04-26 15:46:52 +0900

    Update problem4.py

--- module3/problem4.py
+++ module3/problem4.py
@@ -1,1 +1,5 @@
-
+# Slicing strings
+word = input("Enter a word: ")
+first_index = int(input("Enter the first index: "))
+last_index = int(input("Enter the last index: "))
+print(word[first_index:last_index])
