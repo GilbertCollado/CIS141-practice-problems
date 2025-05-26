c0f18a73778fa739d4a79b0d34836c7bdcaaf360
GILBERT COLLADO DE LA CRUZ <gilbertcolladoxd@outlook.com>
2025-05-25 21:20:56 -0700

    Create problem5.py

---
+++ module7/problem5.py
@@ -0,0 +1,8 @@
+# Write a function called level_up(experience) that takes a player's experience points and returns their new level based on these rules: 0-99 XP → Level 1, 100-199 XP → Level 2, 200+ XP → Level 3
+def level_up(experience):
+    if experience < 100:
+        return 1
+    elif experience < 200:
+        return 2
+    else:
+        return 3
