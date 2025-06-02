9c6dcb375cda983e67e33c5d1d9f5cc1da9aecfa
GILBERT COLLADO DE LA CRUZ <gilbertcolladoxd@outlook.com>
2025-06-01 22:54:51 -0700

    Update problem1.py

--- module8/problem1.py
+++ module8/problem1.py
@@ -1,4 +1,4 @@
-# Create a file called gardening_tips.txt and add at least 3 gardening tips to it. Write a Python script that reads a file gardening_tips.txt and prints out each tip one by one.
+# Write a Python script that reads a file gardening_tips.txt and prints out each tip one by one.
 with open("gardening_tips.txt", "r") as file:
     for tip in file:
         print(tip.strip())
